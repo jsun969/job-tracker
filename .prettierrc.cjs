87a9b821509e8a28a112d5a026ae57629d583413
jsun969 <i@jsun.lol>
2025-02-23 15:23:42 +0800

    :art: Update import order group

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -7,6 +7,6 @@ module.exports = {
 		'@trivago/prettier-plugin-sort-imports',
 		'prettier-plugin-tailwindcss',
 	],
-	importOrder: ['<THIRD_PARTY_MODULES>', '^[./]'],
+	importOrder: ['<THIRD_PARTY_MODULES>', '^~/(.*)$', '^[./]'],
 	importOrderSeparation: true,
 };
