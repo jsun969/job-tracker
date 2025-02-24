2992d2cc340b70f56918d0b7108e547c82bc00b3
jsun969 <i@jsun.lol>
2025-02-24 12:00:28 +0800

    :passport_control: Init better auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
 		<div className="flex h-dvh flex-col items-center justify-center gap-4">
 			<div className="text-4xl font-bold sm:text-6xl">💼 Job Tracker 📊</div>
 			<div className="flex gap-2">
-				<Button>Login with Google</Button>
+				<Button>Login</Button>
 				<Button variant="outline">GitHub</Button>
 			</div>
 		</div>
