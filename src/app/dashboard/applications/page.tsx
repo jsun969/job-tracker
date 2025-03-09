360c19432ec1eacd3c45b12b37f02b79d3b61ac9
jsun969 <i@jsun.lol>
2025-03-09 15:05:28 +1030

    :lipstick: Use  `auto-fill` instead of `auto-fit` to prevent stretching

--- src/app/dashboard/applications/page.tsx
+++ src/app/dashboard/applications/page.tsx
@@ -26,7 +26,7 @@ const ApplicationsPage = async () => {
 								<Icon className="size-6" /> {category}
 							</h2>
 							<Separator className="my-4" />
-							<div className="grid grid-cols-[repeat(auto-fit,_minmax(20rem,_1fr))] gap-2">
+							<div className="grid grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] gap-2">
 								{applications.map((application) => (
 									<ApplicationCard
 										application={application}
