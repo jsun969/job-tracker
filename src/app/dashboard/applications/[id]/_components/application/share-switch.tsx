bb82d9a4eb193271a94aa3aeeae0853b6fed887d
jsun969 <i@jsun.lol>
2025-03-13 17:49:39 +1030

    :sparkles: Info tooltip for share switch

--- src/app/dashboard/applications/[id]/_components/application/share-switch.tsx
+++ src/app/dashboard/applications/[id]/_components/application/share-switch.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { Info } from 'lucide-react';
 import { useOptimisticAction } from 'next-safe-action/hooks';
 
 import { Label } from '~/components/ui/label';
 import { Switch } from '~/components/ui/switch';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from '~/components/ui/tooltip';
 
 import { updateApplication } from '../../../_actions/update-application';
 
@@ -20,7 +26,20 @@ export const ShareSwitch = ({ shared, id }: ShareSwitchProps) => {
 
 	return (
 		<div className="flex items-center gap-2">
-			<Label htmlFor="share">Share</Label>
+			<Label htmlFor="share" className="flex items-center gap-1">
+				Share
+				<Tooltip>
+					<TooltipTrigger>
+						<Info className="size-4" />
+					</TooltipTrigger>
+					<TooltipContent side="bottom">
+						<p className="w-60">
+							Copy the current link and share it with others. Note that
+							recipients must be logged in to access the shared content.
+						</p>
+					</TooltipContent>
+				</Tooltip>
+			</Label>
 			<Switch
 				id="share"
 				checked={action.optimisticState}
